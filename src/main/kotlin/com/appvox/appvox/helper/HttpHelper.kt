56d4167190c6a90b8d7bc8cb1a1f594a027c384a
fabiouu <fab.renoux@gmail.com>
2020-07-05 21:21:05 +0800

    Add HttpHelper to init RestTemplate over proxy

---
+++ src/main/kotlin/com/appvox/appvox/helper/HttpHelper.kt
@@ -0,0 +1,30 @@
+package com.appvox.appvox.helper
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import java.net.InetSocketAddress
+import java.net.Proxy
+
+@Configuration
+@Component
+class HttpHelper(
+
+    @Value("\${scraper.proxy.host:}")
+    private val proxyHost: String,
+
+    @Value("\${scraper.proxy.port:0}")
+    private val proxyPort : Int
+)  {
+
+    fun getRestTemplate() : RestTemplate {
+        val requestFactory = SimpleClientHttpRequestFactory()
+        if (proxyHost.isNotEmpty() && proxyPort != 0) {
+            val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(proxyHost, proxyPort))
+            requestFactory.setProxy(proxy)
+        }
+        return RestTemplate(requestFactory);
+    }
+}
