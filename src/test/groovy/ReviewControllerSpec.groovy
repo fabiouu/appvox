fb6db20b2e9cbf494e85f570f65296ee3536c1b1
fabiouu <fab.renoux@gmail.com>
2020-07-15 23:35:23 +0800

    Remove unneeded parameters from integration tests

--- src/test/groovy/ReviewControllerSpec.groovy
+++ src/test/groovy/ReviewControllerSpec.groovy
@@ -19,31 +19,32 @@ class ReviewControllerSpec extends Specification {
     @Autowired
     TestRestTemplate restTemplate
 
-    def "Get app reviews from Google Play"(String appId, String region, String language, int sortType, int size, int responseStatus, int reviewCount) {
+    def "Get app reviews from Google Play with no token"() {
         when: "we send a request to Google Play for reviews"
-            String urlPattern = "/store/google-play/app/{0}/reviews?region={1}&language={2}&sortType={3}&size={4}"
-            String url = MessageFormat.format(urlPattern, appId, region, language, sortType, size);
-                def resp = restTemplate.getForEntity(url, ReviewsResponse)
+            String urlPattern = "/store/google-play/app/{0}/reviews?language={1}&sortType={2}&size={3}"
+            String url = MessageFormat.format(urlPattern, appId, language, sort, size);
+            def resp = restTemplate.getForEntity(url, ReviewsResponse)
+
+        then: "we should get a #size reviews from #appId app"
+            assert resp.status == responseStatus
+            assert resp.body.reviews.size == reviewCount
+
+        where:
+            appId                   | region | language | sort  | size   ||  responseStatus  | reviewCount
+            "com.twitter.android"   | "us"   | "en"     | 1     | 40     ||  200             | 40
+    }
+
+    def "Get app reviews from App Store"() {
+        when: "we send a request to App Store for reviews"
+            String urlPattern = "/store/app-store/app/{0}/reviews?region={1}&page={2}"
+            String url = MessageFormat.format(urlPattern, appId, region, page);
+            def resp = restTemplate.getForEntity(url, ReviewsResponse)
 
         then: "we should get a #size reviews from #appId app"
             assert resp.status == responseStatus
             assert resp.body.reviews.size == reviewCount
         where:
-            appId                   | region | language | sortType | size   ||  responseStatus  | reviewCount
-            "com.twitter.android"   | "us"   | "en"     | 1        | 40     ||  200             | 40
-    }
-
-    def "Get app reviews from App Store"(String appId, String region, String language, int sortType, int size, int responseStatus, int reviewCount) {
-        when: "we send a request to App Store for reviews"
-        String urlPattern = "/store/app-store/app/{0}/reviews?region={1}&language={2}&sortType={3}&size={4}"
-        String url = MessageFormat.format(urlPattern, appId, region, language, sortType, size);
-        def resp = restTemplate.getForEntity(url, ReviewsResponse)
-
-        then: "we should get a #size reviews from #appId app"
-        assert resp.status == responseStatus
-        assert resp.body.reviews.size == reviewCount
-        where:
-        appId                   | region | language | sortType | size   ||  responseStatus  | reviewCount
-        "333903271"             | "us"   | "en"     | 1        | 0     ||  200              | 10
+            appId       | region    | language  | sort  | page  || responseStatus   | reviewCount
+            "333903271" | "us"      | "en"      | 1     | 0     || 200              | 10
     }
 }
