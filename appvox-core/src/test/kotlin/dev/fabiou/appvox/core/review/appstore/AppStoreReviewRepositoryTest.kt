04dd700239e7e323a53324273fb876616b2310a1
fabiouu <fab.renoux@gmail.com>
2021-04-16 20:15:52 +0800

    Correct failing AppStoreReviewRepository unit test

--- appvox-core/src/test/kotlin/dev/fabiou/appvox/core/review/appstore/AppStoreReviewRepositoryTest.kt
+++ appvox-core/src/test/kotlin/dev/fabiou/appvox/core/review/appstore/AppStoreReviewRepositoryTest.kt
@@ -1,6 +1,7 @@
 package dev.fabiou.appvox.core.review.appstore
 
 import dev.fabiou.appvox.core.BaseRepositoryTest
+import dev.fabiou.appvox.core.app.appstore.AppStoreRepository
 import dev.fabiou.appvox.core.configuration.RequestConfiguration
 import dev.fabiou.appvox.core.review.ReviewRequest
 import dev.fabiou.appvox.core.review.appstore.domain.AppStoreReviewRequest
@@ -12,29 +13,39 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class AppStoreReviewRepositoryTest : BaseRepositoryTest() {
 
+    private var appStoreRepository = AppStoreRepository(RequestConfiguration(delay = 3000))
+
     private val repository = AppStoreReviewRepository(RequestConfiguration(delay = 3000))
 
     @ExperimentalCoroutinesApi
     @ParameterizedTest
     @CsvSource(
-        "333903271, us, 10"
+            "333903271, us, 10"
     )
     fun `Get app store reviews`(appId: String, regionCode: String, requestedSize: Int) {
         AppStoreReviewRepository.REQUEST_URL_DOMAIN =
                 UrlUtil.getUrlDomainByEnv(AppStoreReviewRepository.REQUEST_URL_DOMAIN)
+        AppStoreRepository.APP_HP_URL_DOMAIN =
+                UrlUtil.getUrlDomainByEnv(AppStoreRepository.APP_HP_URL_DOMAIN)
         val region = AppStoreRegion.fromValue(regionCode)
+        stubHttpUrl(AppStoreRepository.APP_HP_URL_PATH.format(region.code, appId), "mock-bearer-token")
         val mockData = javaClass.getResource("/review/app_store/appstore_reviews_mock_data.json").readText()
         stubHttpUrl(
-            AppStoreReviewRepository.REQUEST_URL_PATH.format(
-                region.code, appId,
-                AppStoreReviewRepository.REQUEST_REVIEW_SIZE
-            ), mockData
+                AppStoreReviewRepository.REQUEST_URL_PATH.format(
+                        region.code, appId,
+                        AppStoreReviewRepository.REQUEST_REVIEW_SIZE
+                ), mockData
+        )
+
+        val bearerToken = appStoreRepository.getBearerToken(
+                appId = appId,
+                region = region
         )
 
         val request = AppStoreReviewRequest(
-            appId = appId,
-            region = region,
-            bearerToken = "mock-bearer-token"
+                appId = appId,
+                region = region,
+                bearerToken = bearerToken
         )
 
         val response = repository.getReviewsByAppId(ReviewRequest((request)))
