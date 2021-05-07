39d4df9d11b5aac62a4968deaf8efae28bef8b8e
fabiouu <fab.renoux@gmail.com>
2021-05-08 07:46:26 +0800

    Add Memoization for request tokens

---
+++ appvox-core/src/main/kotlin/dev/fabiou/appvox/util/MemoizeUtil.kt
@@ -0,0 +1,12 @@
+package dev.fabiou.appvox.util
+
+class MemoizeUtil<in FirstParameter, in SecondParameter, out Result>(
+    val f: (FirstParameter, SecondParameter) -> Result
+) : (FirstParameter, SecondParameter) -> Result {
+    private val values = mutableMapOf<FirstParameter, Result>()
+    override fun invoke(x: FirstParameter, y: SecondParameter): Result {
+        return values.getOrPut(x, { f(x, y) })
+    }
+}
+
+fun <FirstParameter, SecondParameter, Result> ((FirstParameter, SecondParameter) -> Result).memoize(): (FirstParameter, SecondParameter) -> Result = MemoizeUtil(this)
