871838457f33ae76e501ce086c169febb1beb2ad
fabiouu <fab.renoux@gmail.com>
2025-06-11 22:27:51 +0800

    Add missing version to Kotlin BOM

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     kotlin("jvm") version "2.1.21" apply true
     `java-library`
     `maven-publish`
-//    signing
 }
 
 group = "com.github.fabiouu.appvox"
@@ -25,10 +24,8 @@ subprojects {
     }
 
     dependencies {
-        implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-//        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.1.21"))
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-        runtimeOnly("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.apache.commons:commons-text:1.13.1")
         testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
         testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
@@ -39,7 +36,7 @@ subprojects {
 
     tasks.withType<KotlinJvmCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_23)
+            jvmTarget.set(JvmTarget.JVM_21)
             freeCompilerArgs.add("-Xjsr305=strict")
         }
     }
@@ -68,76 +65,10 @@ subprojects {
         }
     }
 
-//    publishing {
-//        repositories {
-//            maven {
-//                name = "GitHubPackages"
-//                url = uri("https://maven.pkg.github.com/fabiouu/appvox")
-//                credentials {
-//                    username = System.getenv("GITHUB_USERNAME")
-//                    password = System.getenv("GITHUB_TOKEN")
-//                }
-//            }
-//        }
-//    }
-
     tasks.test {
         useJUnitPlatform()
     }
 }
-
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            artifactId = "appvox-googleplay"
-//            from(components["java"])
-//            versionMapping {
-//                usage("java-api") {
-//                    fromResolutionOf("runtimeClasspath")
-//                }
-//                usage("java-runtime") {
-//                    fromResolutionResult()
-//                }
-//            }
-//            pom {
-//                name.set("AppVox")
-//                description.set("Capture the voice of your App users")
-//                url.set("https://github.com/fabiouu/appvox")
-//                licenses {
-//                    license {
-//                        name.set("The Apache License, Version 2.0")
-//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                    }
-//                }
-//                developers {
-//                    developer {
-//                        id.set("https://github.com/fabiouu")
-//                        name.set("fabiouu")
-//                        email.set("fab.renoux@gmail.com")
-//                    }
-//                }
-//                scm {
-//                    connection.set("scm:git:git@github.com:fabiouu/appvox.git")
-//                    developerConnection.set("scm:git:git@github.com:fabiouu/appvox.git")
-//                    url.set("https://github.com/fabiouu/appvox/tree/master")
-//                }
-//            }
-//            repositories {
-//                maven {
-//                    val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
-//                    val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
-//                    url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-//                }
-//            }
-//        }
-//    }
-//}
-
-//signing {
-//    sign(publishing.publications["mavenJava"])
-//}
-
-
 
 tasks.javadoc {
     if (JavaVersion.current().isJava9Compatible) {
